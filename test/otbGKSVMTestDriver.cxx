c1a3c6d56f05da9c59271d3497ff1514c05bf087
Julien Michel <julien.michel@orfeo-toolbox.org>
2015-03-24 11:46:40 +0100

    TEST: Removing tests depending on other classes than SVMModel

--- test/otbGKSVMTestDriver.cxx
+++ test/otbGKSVMTestDriver.cxx
@@ -5,8 +5,5 @@ void RegisterTests()
   REGISTER_TEST(otbGKSVMModelCopyTest);
   REGISTER_TEST(otbGKSVMModelGenericKernelLoadSave);
   REGISTER_TEST(otbGKSVMModelGenericKernelsTest);
-  REGISTER_TEST(otbSVMKernelsTest);
-  REGISTER_TEST(otbSVMInverseCosSpectralAngleKernelFunctorImageModelEstimatorTest);
-  REGISTER_TEST(otbSVMInverseCosSpectralAngleKernelFunctorImageClassificationTest);
   REGISTER_TEST(otbGKSVMModelCopyComposedKernelTest);
 }
