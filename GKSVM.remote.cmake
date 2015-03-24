553dc8e40ab1538c46de6596ec323627dac5fea5
Julien Michel <julien.michel@orfeo-toolbox.org>
2015-03-24 12:00:24 +0100

    DOC: Adding revision to remote.cmake file

--- GKSVM.remote.cmake
+++ GKSVM.remote.cmake
@@ -3,5 +3,5 @@
 otb_fetch_module(GKSVM
   "Patched version of LibSVM and dedicated OTB classes to provide generic kernels for SVM algorithm"
   GIT_REPOSITORY https://github.com/jmichel-otb/GKSVM.git
-  GIT_TAG 
+  GIT_TAG 4298da82ff781d806658b191a9bc68f46ce9c948
   )
